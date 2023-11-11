a7ff5ee0a5a97a80ac9ac3b433ea2adf5f86e3da
Жава <63841611+javamaks@users.noreply.github.com>
2023-11-11 23:57:41 +0500

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
-﻿#include <SFML/Graphics.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Network.hpp>
+#include <string.h>
 #include <time.h>
 #include <vector>
 #include <iostream>
@@ -10,6 +13,22 @@ int main()
     RenderWindow window(VideoMode(1000, 600), "Dino");
     srand(time(NULL));
 
+    SoundBuffer buffer;
+    buffer.loadFromFile("images/dino.mp3");
+    Sound sound;
+    sound.setBuffer(buffer);
+    sound.play();
+
+
+    //Добавление текстуры птеродактиль
+    Texture Pterodactyl;
+    Pterodactyl.loadFromFile("images/BirdSpriteSheet.png");
+    Sprite pterodact(Pterodactyl);
+    Sprite pterodact1(Pterodactyl);
+    pterodact.setTextureRect(IntRect(0, 4, 95, 79));
+    pterodact.setPosition(900, 170);
+
+    //Добавление текстуры динозаврика
     Texture dinoTexture;
     dinoTexture.loadFromFile("images/PlayerSpriteSheet.png");
     Texture platoTexture;
@@ -18,21 +37,25 @@ int main()
     cactusTexture[0].loadFromFile("images/Cactus1.png");
     cactusTexture[1].loadFromFile("images/Cactus2.png");
     cactusTexture[2].loadFromFile("images/Cactus3.png");
-
+    //Добавление шрифта
     Font font;
     font.loadFromFile("images/Font8bit.ttf");
-
+    //Добавление текста
     Text text;
     text.setFont(font);
     text.setCharacterSize(24);
     text.setFillColor(Color::Black);
     text.setString("DINO");
-
     text.setPosition(window.getSize().x - text.getLocalBounds().width - 10, 10);
 
     Sprite player_stand;
     Sprite plato1;
     Sprite plato2;
+
+    float gravity = 0.5f;
+    float jumpStrength = -10.0f;
+    float initialY = 300.0f;
+    float yVelocity = 0.0f;
 
     bool isJumping = false;
     float jumpHeight = 100.0f;
@@ -53,10 +76,12 @@ int main()
     Clock gameClock;
     bool isRunning = false;
     bool spacePressed = false;
+    vector<Sprite> cactiOnPlatform;
 
-    vector<Sprite> cactiOnPlatform; // Вектор для хранения кактусов
-
-    int cactusInterval = 300;
+    /*int PterodactylInterval = 600;
+    int PterodactylTimer = 0;
+    */
+    int cactusInterval = 600;
     int cactusTimer = 0;
 
     while (window.isOpen()) {
@@ -70,12 +95,10 @@ int main()
                 window.close();
             }
             if (event.type == Event::KeyPressed) {
-                if (event.key.code == Keyboard::Left) {
-                    isRunning = true;
-                }
                 if (event.key.code == Keyboard::Space && !isJumping) {
                     isJumping = true;
                     spacePressed = true;
+                    yVelocity = jumpStrength;
                 }
             }
         }
@@ -102,9 +125,14 @@ int main()
                 if (currentSprite >= 3) {
                     currentSprite = 0;
                 }
+                if (currentSprite >= 2) {
+                    currentSprite = 0;
+                }
                 timeSinceLastChange = 0;
             }
             player_stand.setTextureRect(IntRect(currentSprite * 98, 0, 98, 110));
+            pterodact.setTextureRect(IntRect(currentSprite * 98, 0, 98, 110));
+
         }
 
         if (isRunning) {
@@ -124,6 +152,7 @@ int main()
 
         plato1.move(-0.2 * elapsedTime, 0);
         plato2.move(-0.2 * elapsedTime, 0);
+        pterodact.move(-0.2 * elapsedTime, 0);
 
         if (plato1.getPosition().x <= -plato1.getGlobalBounds().width) {
             plato1.setPosition(plato2.getPosition().x + plato2.getGlobalBounds().width, 400);
@@ -143,11 +172,13 @@ int main()
             cactusTimer = 0;
         }
 
+
         window.clear();
         window.draw(plato1);
         window.draw(plato2);
         window.draw(player_stand);
         window.draw(text);
+        window.draw(pterodact);
 
         for (int i = 0; i < cactiOnPlatform.size(); i++) {
             cactiOnPlatform[i].move(-0.1 * elapsedTime, 0);
